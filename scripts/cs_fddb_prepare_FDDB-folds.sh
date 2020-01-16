cfcca0391dc02274eb996c19e041b93df5923821
cezs <diagram.chasing@gmail.com>
2020-01-16 00:27:26 +0000

    Remove redundant files

--- scripts/cs_fddb_prepare_FDDB-folds.sh
+++
@@ -1,8 +0,0 @@
-mkdir FDDB-folds/annotations
-mkdir FDDB-folds/paths
-
-mv FDDB-folds/FDDB-fold-*-ellipseList.txt FDDB-folds/annotations
-mv FDDB-folds/FDDB-fold-*.txt FDDB-folds/paths
-
-cat FDDB-folds/annotations/FDDB-fold-*-ellipseList.txt >> FDDB-folds/FDDB-annotations.txt
-cat FDDB-folds/paths/FDDB-fold-*.txt >> FDDB-folds/FDDB-paths.txt
